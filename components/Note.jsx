7038370adf5b3cd8f73542d9e684a95fe1cdb5ed
prankshadow <padmesh.sharma26@gmail.com>
2023-07-02 17:14:21 +0530

    added ADD ITEM

--- components/Note.jsx
+++ components/Note.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 
 const Note = () => {
     return (
-        <div className='px-20 py-8'>
-            <label>
+        <form className='px-20 py-8 flex justify-items-end'>
+            <label className='w-full'>
                 <span className='font-satoshi font-semibold text-4xl text-grey-700 font-alegreyaSansSC'>Add Item</span>
                 <input required
                     className='todo_input'
                 />
             </label>
-        </div>
+
+            <div className='flex items-center'>
+                <button className='border-2 border-solid border-black rounded-md font-alegreyaSansSC font-bold text-2xl px-6 py-2'>
+                    Add
+                </button>
+            </div>
+
+        </form>
     )
 }
 
--- components/ToDoItem.jsx
+++ components/ToDoItem.jsx
@@ -3,10 +3,7 @@ import React from 'react'
 const ToDoItem = () => {
     return (
         <main className='note'>
-            <label>
-                <span className='font-satoshi font-semibold text-base text-grey-700'>Tag{` `}<span className='font-normal'>(#product, #webdevelopment, #idea)</span></span>
-            </label>
-            <button>Add</button>
+        
         </main>
     )
 }
