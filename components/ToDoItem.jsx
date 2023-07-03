da82fdd7f22b45bd5b24ca84325e6ed821c9f2d7
prankshadow <padmesh.sharma26@gmail.com>
2023-07-03 17:34:34 +0530

    done with completed task func

--- components/ToDoItem.jsx
+++ components/ToDoItem.jsx
@@ -1,19 +1,42 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const ToDoItem = ({ post }) => {
+
+
+    const [isCompleted, setIsCompleted] = useState(false);
+    // const [notCompleted, setNotCompleted] = useState(true);
+
+    const handleCheck = () => {
+        setIsCompleted((prev) => !prev);
+    };
+
+    // const handleCross = () => {
+    //     setNotCompleted((prev) => !prev)
+    // }
+
+
     return (
         <div className='text-4xl font-alegreyaSansSC font-semibold'>
             <div className='flex items-center'>
-                <h1 className='heading_property px-8 grow-0 text-ellipsis overflow-hidden w-[80%] hover:break-all'>{post.todoitem}</h1>
+                <h1 className={`heading_property px-8 grow-0 text-ellipsis overflow-hidden w-[80%] hover:break-all ${isCompleted ? 'line-through decoration-4' : ''}`}>{post.todoitem}</h1>
                 <div className='grid grid-cols-2 gap-3'>
                     <button>
-                        <Image
-                            src='/check.svg'
+                        {isCompleted ? <Image
+                            src='/cross.svg'
                             width={18}
                             height={18}
-                            alt='check button'
-                        />
+                            alt='cross button'
+                            onClick={handleCheck}
+                        /> :
+                            <Image
+                                src='/check.svg'
+                                width={18}
+                                height={18}
+                                alt='check button'
+                                onClick={handleCheck}
+                            />}
                     </button>
                     <button>
                         <Image
