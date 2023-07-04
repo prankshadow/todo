82e8a11975bc4c9f9ef306dadb43cfde8d65f365
prankshadow <padmesh.sharma26@gmail.com>
2023-07-04 16:05:45 +0530

    Working on FrontEnd///////Profile section done

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,40 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
   return (
     <div className='pt-8 w-full'>
-        <h1 className='text-center font-fasthand text-6xl orange_gradient'>To-Do List</h1>
+      <h1 className='text-center font-fasthand text-6xl orange_gradient'>To-Do List</h1>
+
+      <div className='absolute md:right-10 top-10 right-3 lg:block md:block hidden'>
+        <button>
+
+          <Image src='/logo.svg'
+            width={37}
+            height={37}
+            className="rounded-full"
+            alt="profile"
+          />
+        </button>
+      </div>
+
+      {/* Mobile navigation */}
+
+      <div className='absolute md:right-10 top-10 right-3 lg:hidden md:hidden block'>
+        <button>
+
+          <Image src='/logo2.svg'
+            width={37}
+            height={37}
+            className="rounded-full"
+            alt="profile"
+          />
+        </button>
+      </div>
+
     </div>
   )
 }
 
 export default Header
+
