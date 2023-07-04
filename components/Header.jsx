aab6c0b2e3753ed54b64999ea7f0a26cdd154448
prankshadow <padmesh.sharma26@gmail.com>
2023-07-04 17:59:46 +0530

    done with user profile

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,35 +1,122 @@
+"use client"
 import Image from 'next/image'
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Header = () => {
+
+  const isUserLoggedIn = true
+
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false)
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    }
+
+    setProviders();
+  })
+
+
   return (
     <div className='pt-8 w-full'>
       <h1 className='text-center font-fasthand text-6xl orange_gradient'>To-Do List</h1>
 
       <div className='absolute md:right-10 top-10 right-3 lg:block md:block hidden'>
-        <button>
 
-          <Image src='/logo.svg'
-            width={37}
-            height={37}
-            className="rounded-full"
-            alt="profile"
-          />
-        </button>
+        {isUserLoggedIn ? (
+          <button>
+            <Image src='/logo.svg'
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+
+
+          </button>
+        ) : (
+          <>
+            {providers && Object.values(providers).map((provider) => (
+              <button
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className='black_btn'
+              >
+                Sign In
+              </button>
+            ))}
+          </>
+        )}
+
       </div>
 
       {/* Mobile navigation */}
 
       <div className='absolute md:right-10 top-10 right-3 lg:hidden md:hidden block'>
-        <button>
 
-          <Image src='/logo2.svg'
-            width={37}
-            height={37}
-            className="rounded-full"
-            alt="profile"
-          />
-        </button>
+        {isUserLoggedIn ? (
+          <button>
+            <Image src='/logo2.svg'
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+
+
+          </button>
+        ) : (
+          <>
+            {providers && Object.values(providers).map((provider) => (
+              <button
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className='black_btn'
+              >
+                Sign In
+              </button>
+            ))}
+          </>
+        )}
+
       </div>
 
     </div>
