c3571e2d85f60fdf85a3517111b03629047b7341
prankshadow <padmesh.sharma26@gmail.com>
2023-07-05 13:30:46 +0530

    fixed some errors

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,38 +9,39 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_ID,
         })
-    ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        session.user.id = sessionUser._id.toString();
-        return session;
-    },
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
-
-            //If user already exist
-            const userExists = await User.findOne({
-                email: profile.email
+    ], callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
             })
 
-            //If not, create user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
+            session.user.id = sessionUser._id.toString();
+            return session;
+        },
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
+
+                //If user already exist
+                const userExists = await User.findOne({
+                    email: profile.email
                 })
+
+                //If not, create user
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+                return true
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-            return true
-        } catch (error) {
-            console.log(error);
-            return false;
         }
-    },
+    }
 })
 
 
