11727f832830bc50ebbdac5b5ca34621498ea337
prankshadow <padmesh.sharma26@gmail.com>
2023-07-05 17:40:34 +0530

    Sign in to use todo

--- components/Note.jsx
+++ components/Note.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useSession } from 'next-auth/react';
 
 const Note = ({
     post, setPost, submitting, handleSubmit, type,
 }) => {
+
+    const { data: session } = useSession();
+
     return (
         <form className='px-20 py-8 flex justify-items-end' onSubmit={handleSubmit}>
             <label className='w-full'>
@@ -11,12 +15,14 @@ const Note = ({
                     className='todo_input'
                     value={post.todoitem}
                     onChange={(e) => setPost({ ...post, todoitem: e.target.value })}
+                    disabled={!session}
+                    placeholder={session ? '' : 'PLEASE SIGN IN WITH GOOGLE'}
                 />
             </label>
 
             <div className='flex items-center'>
                 <button type='submit' className='border-2 border-solid border-black rounded-md font-alegreyaSansSC font-bold text-2xl px-6 py-2'
-                    disabled={submitting}
+                    disabled={!session || submitting}
                 >
                     {submitting ? `${type}...` : type}
                 </button>
